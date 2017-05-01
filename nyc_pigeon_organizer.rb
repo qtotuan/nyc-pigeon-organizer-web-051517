13d6093e46f2650b2c939a814b45fb8df2337b08
Quynh To Tuan <quynh.totuan@gmail.com>
2017-05-01 12:40:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |category, detail_hash|
+    detail_hash.each do |attribute, name_array|
+      name_array.each do |name|
+        if hash[name].nil?
+          hash[name] = {"#{category}": [attribute.to_s]}
+        elsif hash[name].keys.include?(category)
+          hash[name][category] << attribute.to_s
+        else
+          hash[name][category] = [attribute.to_s]
+        end
+      end
+    end
+  end
+  hash
 end
